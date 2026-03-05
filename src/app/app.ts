2bc2011d3359b003e010340ce2654a669f630d2f
Denis Espanhol <denisgfespanhol@gmail.com>
2026-03-05 14:44:24 -0300

    Inserting mypage component in app

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,10 +1,11 @@
 import { Component, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { MypageComponent } from './mypage/mypage.component';
 import { HelloWorldComponent } from './helloworld/helloworld.component';
 
 @Component({
   selector: 'app-root',
-  imports: [HelloWorldComponent],
+  imports: [HelloWorldComponent, MypageComponent],
   templateUrl: './app.html',
   styleUrl: './app.css'
 })
--- src/app/mypage/mypage.component.ts
+++ src/app/mypage/mypage.component.ts
@@ -1,7 +1,7 @@
 import { Component } from '@angular/core';
 
 @Component({
-  selector: 'app-mypage.component',
+  selector: 'app-mypage',
   imports: [],
   templateUrl: './mypage.component.html',
   styleUrl: './mypage.component.css',
