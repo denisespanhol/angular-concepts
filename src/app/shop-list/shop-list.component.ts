84fe35e437051217703473648a34c4a4dab2c4d4
Denis Espanhol <denisgfespanhol@gmail.com>
2026-03-09 11:02:58 -0300

    Creating Shop-List html and css

--- src/app/shop-list/shop-list.component.ts
+++ src/app/shop-list/shop-list.component.ts
@@ -1,9 +1,17 @@
 import { Component } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-shop-list',
-  imports: [],
+  imports: [FormsModule],
   templateUrl: './shop-list.component.html',
   styleUrl: './shop-list.component.css',
 })
-export class ShopList {}
+export class ShopList {
+  newItem: string = '';
+
+  addItemToList() {
+    console.log(this.newItem);
+    this.newItem = '';
+  }
+}
